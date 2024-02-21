b479027b21c43916ceff03ecc00a9d987c04b840
Vitor de Paula <vitordepaula98@hotmail.com>
2024-02-21 18:14:58 -0300

    instalado e configurado react-router-dom e criado duas rotas

--- src/components/Button.styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const ButtonContainer = styled.button`
-  width: 100px;
-  height: 40px;
-
-  background-color: ${(props) => props.theme['green-500']};
-`
