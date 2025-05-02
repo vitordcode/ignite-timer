e35f51f64df349d049c4c2f77e84ab6bab6c1e74
Vitor de Paula <vitordepaula98@hotmail.com>
2025-05-02 17:38:23 -0300

    instalado e configurado styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from "./components/Button";
+
 export function App() {
-	return <h1>Ola mundo</h1>;
+	return (
+		<>
+			<Button variant="primary" />
+			<Button variant="secondary" />
+			<Button variant="success" />
+			<Button variant="danger" />
+			<Button />
+		</>
+	);
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer, type ButtonVariant } from "./Button.styles";
+
+interface ButtonProps {
+	variant?: ButtonVariant;
+}
+
+export function Button({ variant = "primary" }: ButtonProps) {
+	return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
+}
