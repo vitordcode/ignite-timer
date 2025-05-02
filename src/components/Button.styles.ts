723e34de883219de36ad95893b8a7f82c406a833
Vitor de Paula <vitordepaula98@hotmail.com>
2025-05-02 19:14:52 -0300

    configurado tema global e paleta de cores

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,8 +16,10 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
 	width: 100px;
 	height: 40px;
+	border-radius: 4px;
+	border: 0;
+	margin: 8px;
 
-	${(props) => {
-		return `background-color: ${buttonVariants[props.variant]}`;
-	}}
+	background-color: ${(props) => props.theme["green-500"]};
+	color: ${(props) => props.theme.white};
 `;
