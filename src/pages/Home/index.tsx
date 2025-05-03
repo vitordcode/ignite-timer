7bab88d3cb9cd9125cd5f8fff2d4e174d9de8fba
Vitor de Paula <vitordepaula98@hotmail.com>
2025-05-03 17:28:48 -0300

    Criado estrutura e estilo da página History

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,14 +18,23 @@ export function Home() {
 					<TaskInput
 						type="text"
 						id="task"
+						list="task-suggestions"
 						placeholder="Dê um nome para o seu projeto"
 					/>
+					<datalist id="task-suggestions">
+						<option value="Projeto 1" />
+						<option value="Projeto 2" />
+						<option value="Projeto 3" />
+					</datalist>
 
 					<label htmlFor="minutesAmount">Durante</label>
 					<MinutesAmountInput
 						type="number"
 						id="minutesAmount"
 						placeholder="00"
+						min={5}
+						max={60}
+						step={5}
 					/>
 
 					<span>minutos.</span>
